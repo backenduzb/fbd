0e9bc43546da66bbf792b8f1cfef704ceb33a210
Javohir <qobiljonsharipov33@gmail.com>
2025-12-23 15:32:48 +0500

    Maked entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,8 @@
+set -e
+
 python manage.py makemigrations --no-input
-python manage.py migrate --not-input
+python manage.py migrate --no-input
 
-python manage.py collectstatic
+python manage.py collectstatic --no-input
 
+gunicorn config.wsgi:application -w 4 -b 0.0.0.0:8000
