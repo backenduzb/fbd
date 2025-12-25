1832078658551aeb84f8b14222d477cf733aa71a
Javohir <qobiljonsharipov33@gmail.com>
2025-12-25 20:10:43 +0500

    Edtited view page

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,6 @@ python manage.py migrate --no-input
 
 python manage.py collectstatic --no-input
 
-python manage.py makeadmin --no-input
+python manage.py makeadmin 
 
 gunicorn config.wsgi:application -w 4 -b 0.0.0.0:8000
