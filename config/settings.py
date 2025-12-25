2dd6a482dd8e7197dc060e08d9358807cd9c0b2c
Javohir <qobiljonsharipov33@gmail.com>
2025-12-25 20:15:10 +0500

    Edtited view page

--- config/settings.py
+++ config/settings.py
@@ -128,7 +128,7 @@ STATIC_ROOT = os.path.join(BASE_DIR / 'static')
 
 
 MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR / 'media')
+MEDIA_ROOT = [os.path.join(BASE_DIR / 'media')]
 
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 
